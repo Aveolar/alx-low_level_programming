6b1e7ecc7c3562502cae2e20c29381d44ae11432
Aveolar <ogunsanya.adeola71@gmail.com>
2022-03-10 05:33:24 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -21,8 +21,8 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", a);
 	printf("Size of an int: %d byte(s)\n", b);
 	printf("Size of a long int: %d byte(s)\n", c);
-	printf("Size of long long int: %d byte(s)\n", sizeof(d));
-	printf("Size of float: %d byte(s)\n", sizeof(e));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
 
 	return (0);
 }
