aa994d9f15a49133e1d93397a55d66f3c3b7523f
Aveolar <ogunsanya.adeola71@gmail.com>
2022-03-10 08:28:43 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - print to stderr
@@ -7,6 +8,6 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+return (1);
 }
