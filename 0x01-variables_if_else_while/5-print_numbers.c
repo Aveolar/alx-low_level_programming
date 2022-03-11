1b758d27b255b27e0c31ef69e0da769253b524ad
Aveolar <ogunsanya.adeola71@gmail.com>
2022-03-11 16:35:37 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int t;
+
+	t = 0;
+	while (t < 10)
+	{
+		printf("%d", t);
+		t++;
+	}
+	putchar('\n');
+	return (0);
+}
