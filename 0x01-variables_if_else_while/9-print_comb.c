25654ab93b724bf51c7c4aba773e6c1d36581d83
Aveolar <ogunsanya.adeola71@gmail.com>
2022-03-11 18:13:38 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	a = '0';
+	b = ',';
+	c = ' ';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		putchar(b);
+		putchar(c);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
