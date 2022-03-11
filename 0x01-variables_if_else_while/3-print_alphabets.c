c589a443212c38e5354e261b25541dd3fe390f07
Aveolar <ogunsanya.adeola71@gmail.com>
2022-03-11 13:29:12 +0000

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - alphABET
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch, ca;
+
+	ch = 'a';
+	ca = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	while (ca <= 'Z')
+	{
+		putchar(ca);
+		ca++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
