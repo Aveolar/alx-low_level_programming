3f395be0124e8d053975b92794d1270a48371b36
Aveolar <ogunsanya.adeola71@gmail.com>
2022-03-15 19:45:22 +0000

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,12 @@
 
 /**
  * _islower - write a function that checks for lowercase char
- *
+ * @c: parameter for the _islower function
  * Return: Always 0 (Success)
  */
 int _islower(int c)
 {
-	if (islower(c))
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
