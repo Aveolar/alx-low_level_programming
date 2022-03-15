9b353600ff60058a4793146d6dcc3582db9fb868
Aveolar <ogunsanya.adeola71@gmail.com>
2022-03-15 18:28:49 +0000

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - print lowercase alphabet
+ * print_alphabet - print lowercase alphabet
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
-	int low;
+	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
 	{
@@ -15,5 +15,4 @@ int main(void)
 	}
 	_putchar('\n');
 
-	return (0);
 }
