9536296ae9fe1b868b27750db05c1721bd92367b
Aveolar <ogunsanya.adeola71@gmail.com>
2022-03-17 12:38:12 +0000

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function to print 0-9 excluding 2&4 followed by newline
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == 2)
+			continue;
+		else if (i == 4)
+			continue;
+		else
+			_putchar(i);
+	}
+	_putchar('\n');
+}
