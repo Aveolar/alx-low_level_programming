fc9944b2cfa8150f0855c0cfc064d38f2fdab037
Aveolar <ogunsanya.adeola71@gmail.com>
2022-03-17 19:43:25 +0000

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if (i % 15 == 0)
 		{
 			printf("FizzBuzz ");
 		}
@@ -27,8 +27,8 @@ int main(void)
 		{
 			printf("%d ", i);
 		}
+
 	}
-	printf("Buzz");
 	printf("\n");
 	return (0);
 }
