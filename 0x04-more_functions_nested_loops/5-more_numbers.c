65c1463a954346f13672cc48033c455991987f7f
Aveolar <ogunsanya.adeola71@gmail.com>
2022-03-17 13:40:31 +0000

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that print 0-14 10times followed by newline
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int c;
+	char i;
+
+	for (c = 0; c < 10; c++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if ((i / 10) > 0)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+	}
+	_putchar('\n');
+}
