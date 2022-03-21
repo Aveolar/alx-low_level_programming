920a1c4ffa3276140968c76d3096e368c201469e
Aveolar <ogunsanya.adeola71@gmail.com>
2022-03-21 18:39:06 +0000

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - function that return the length of a string
+ *
+ * @s: _strlen function parameter
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	c = 0;
+
+	while (*s[c] != '\0')
+	{
+		c++;
+	}
+
+	return (c);
+}
