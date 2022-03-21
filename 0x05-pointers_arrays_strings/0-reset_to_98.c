d746ff11ab1ac1369d32108aa8473ef086dde28a
Aveolar <ogunsanya.adeola71@gmail.com>
2022-03-21 17:45:50 +0000

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - function that takes a pointer to an int
  *
- * @*n: reset function parameter
+ * @n: reset function parameter
  * Return: Always 0
  */
 void reset_to_98(int *n)
