c356da8d56299ecb8467683313f67017481564b1
Aveolar <ogunsanya.adeola71@gmail.com>
2022-03-21 21:33:28 +0000

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - prints number of array elements
+ * @n: function parameter
+ * @a: function parameter
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n - 1; i++)
+	{
+		printf("%d", *(a + i));
+
+		if (i < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
