ba86641774dde487a2aee84faa2af6ba5cc1cd8b
Aveolar <ogunsanya.adeola71@gmail.com>
2022-03-23 17:31:25 +0000

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string.
+ * @dest: function parameter 1
+ * @src: function parameter 2
+ * @n: function parameter 3
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
