6f33b17b4305138d88aafc9efde4c6cf887fcf3e
Aveolar <ogunsanya.adeola71@gmail.com>
2022-03-31 03:07:55 +0000

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that returns the value of x raised to the pow of y
+ *
+ * @x: funtion parameter
+ * @y: funtion parameter
+ * Return: -1 if function less than 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	if (x == 0)
+		return (0);
+
+	if (x == 1 || y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
