859a462a84ee2daa0301fcb2ce0214f54e43a3c9
Aveolar <ogunsanya.adeola71@gmail.com>
2022-04-11 23:43:02 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,11 @@
 #define _DOG_H_
 
 /**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
  * struct dog - Struct to define a dog
  * @name: name of dog
  * @owner: name's owner of dog
@@ -15,5 +20,10 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
