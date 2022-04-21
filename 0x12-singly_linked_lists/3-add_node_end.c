cfbf792e888a28b8fc7ff416a27c781909a7a75b
Aveolar <ogunsanya.adeola71@gmail.com>
2022-04-21 14:44:56 +0000

    2-add_node.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -15,19 +15,18 @@ int _strlen(const char *str)
 }
 
 /**
- * add_node - add node to beginning of linked list
- * @head: linked list head
+ * add_node_end - add node to end of linked list
+ * @head: linked list
  * @str: data for new node
  * Return: address of new element, or NULL if failed
  */
-
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node; /* create new node */
+	list_t *new_node, *tmp; /* create new node */
 
 	if (str == NULL) /* validate input */
 		return (NULL);
-	if (strdup(str) == NULL) /*check if strdup malloc errored */
+	if (strdup(str) == NULL) /* check if malloc errored */
 		return (NULL);
 
 	new_node = malloc(sizeof(list_t)); /* malloc for new node */
@@ -36,13 +35,18 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node->str = strdup(str); /* set node values */
 	new_node->len = _strlen(str);
+	new_node->next = NULL;
 
-	if (head == NULL) /* set what new node points to first */
-		new_node->next = NULL;
+	if (*head == NULL) /* if no list nodes, set new_node to beginning */
+		*head = new_node;
 	else
-		new_node->next = *head;
+	{
+		tmp = *head;
 
-	*head = new_node; /* set head to point to new node */
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new_node;
+	}
 
 	return (new_node);
 }
